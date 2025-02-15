8163fdeb9037428db91331d8a735b102bfcdb8d9
worldmaker <worldmaker18349276@gmail.com>
2025-02-15 14:14:40 +0800

    refactor as a struct

--- src/incr.rs
+++ src/incr.rs
@@ -122,63 +122,85 @@ const CORNER4: Point = (-f32::INFINITY,  f32::INFINITY);
 const MIN_WIDTH: f32 = 1e-5;
 const HASH_SIZE: u32 = 8;
 
-pub fn delaunay(points: &[Point]) -> Vec<[usize; 3]> {
-    assert!(points.iter().all(|&(x, y)| x.is_finite() && y.is_finite()));
-    
-    // number of given points
-    let len = points.len();
-    // given points and four bounding box points
-    let points = {
-        // normalize
-        let x_min = points.iter().map(|p| p.0).reduce(|a, b| a.min(b)).unwrap_or(0.0);
-        let y_min = points.iter().map(|p| p.1).reduce(|a, b| a.min(b)).unwrap_or(0.0);
-        let x_max = points.iter().map(|p| p.0).reduce(|a, b| a.max(b)).unwrap_or(0.0);
-        let y_max = points.iter().map(|p| p.1).reduce(|a, b| a.max(b)).unwrap_or(0.0);
-        let width = (x_max - x_min).max(y_max - y_min).max(MIN_WIDTH);
-        points.iter()
-            .map(|&(x, y)| ((x - x_min) / width, (y - y_min) / width))
-            .chain([CORNER1, CORNER2, CORNER3, CORNER4])
-            .collect::<Vec<_>>()
-    };
-    let hashes = points.iter().map(|&(x, y)| hilbert_dist::<HASH_SIZE>(x, y)).collect::<Vec<_>>();
-
-    const BD: usize = usize::MAX;
-    // by Euler characteristic, the maximal number of faces of triangluation of V points is 2V - 6
-    // (assume it has rectangle convex hull)
-    let triangle_capacity = len * 2 + 2;
+struct Delaunay {
+    points: Vec<Point>,
     // [a_index, b_index, c_index]
-    let mut triangles = Vec::<[usize; 3]>::with_capacity(triangle_capacity);
-    triangles.push([len+0, len+1, len+2]);
-    triangles.push([len+2, len+3, len+0]);
+    triangles: Vec<[usize; 3]>,
     // [bc_adj, ca_adj, ab_adj],  adj = tri_index * 3 + side or BD for unmovable edge
-    let mut adjacencies: Vec<[usize; 3]> = vec![[BD, 1*3+1, BD], [BD, 0*3+1, BD]];
-    adjacencies.reserve(len * 2);
+    adjacencies: Vec<[usize; 3]>,
+    // (bottom-left point, width)
+    bbox: (Point, f32),
+    // point_index -> hash
+    hashes: Vec<u32>,
     // hash -> tri_index,  each hash indicates a grid region
-    let mut grid: BTreeMap<u32, usize> = BTreeMap::new();
-    grid.insert(hashes[len+0], 0);
-    grid.insert(hashes[len+1], 0);
-    grid.insert(hashes[len+2], 1);
-    grid.insert(hashes[len+3], 1);
+    grid: BTreeMap<u32, usize>,
+}
 
-    for p in 0..len {
-        let point = &points[p];
-        let hash = hashes[p];
+impl Delaunay {
+    const BD: usize = usize::MAX;
+
+    pub fn new(bbox: (Point, f32), capacity: usize) -> Self {
+        let mut res = Self {
+            bbox,
+            points: Vec::with_capacity(capacity + 4),
+            // by Euler characteristic, the maximal number of faces of triangluation of V points is 2V - 6
+            // (assume it has rectangle convex hull)
+            triangles: Vec::with_capacity(capacity * 2 + 2),
+            adjacencies: Vec::with_capacity(capacity * 2),
+            hashes: Vec::with_capacity(capacity + 4),
+            grid: BTreeMap::new(),
+        };
+
+        // add four bounding box points
+        res.points.push(CORNER1);
+        res.points.push(CORNER2);
+        res.points.push(CORNER3);
+        res.points.push(CORNER4);
+
+        res.triangles.push([0, 1, 2]);
+        res.triangles.push([2, 3, 0]);
+        res.adjacencies.push([Self::BD, 1*3+1, Self::BD]);
+        res.adjacencies.push([Self::BD, 0*3+1, Self::BD]);
+
+        res.hashes.push(res.hash(&res.points[0]));
+        res.hashes.push(res.hash(&res.points[1]));
+        res.hashes.push(res.hash(&res.points[2]));
+        res.hashes.push(res.hash(&res.points[3]));
+
+        res.grid.insert(res.hashes[0], 0);
+        res.grid.insert(res.hashes[1], 0);
+        res.grid.insert(res.hashes[2], 1);
+        res.grid.insert(res.hashes[3], 1);
+        
+        res
+    }
+    
+    fn hash(&self, &(x, y): &Point) -> u32 {
+        let ((x0, y0), w) = &self.bbox;
+        hilbert_dist::<HASH_SIZE>((x - x0) / w, (y - y0) / w)
+    }
+
+    pub fn add(&mut self, point: &Point) {
+        let hash = self.hash(point);
+        let p = self.points.len();
+        self.points.push(*point);
+        self.hashes.push(hash);
 
         // find triangle contains points[i]
         let t = {
             // find the nearest triangle in the grid
             let mut t = *min_by_key(
-                grid.range(hash..).next(),
-                grid.range(..hash).next_back(),
+                self.grid.range(hash..).next(),
+                self.grid.range(..hash).next_back(),
                 |e| e.map(|(h, _)| h.abs_diff(hash)).unwrap_or(u32::MAX),
             ).unwrap().1;
             // walk to the point
             loop {
-                let [a, b, c] = triangles[t];
-                let side = triside(&points[a], &points[b], &points[c], point);
+                let [a, b, c] = self.triangles[t];
+                let side = triside(&self.points[a], &self.points[b], &self.points[c], point);
                 if let Triside::Inside = side { break; }
-                let edge = adjacencies[t][side as usize];
-                if edge == BD {
+                let edge = self.adjacencies[t][side as usize];
+                if edge == Self::BD {
                     panic!("invalid");
                 }
                 t = edge / 3;
@@ -188,29 +210,29 @@ pub fn delaunay(points: &[Point]) -> Vec<[usize; 3]> {
 
         // divide triangle
         let (e1, e2, e3) = {
-            let [a, b, c] = triangles[t];
-            let [bc, ca, ab] = adjacencies[t];
+            let [a, b, c] = self.triangles[t];
+            let [bc, ca, ab] = self.adjacencies[t];
             // indices for new triangles
             let t1 = t;
-            let t2 = triangles.len();
-            let t3 = triangles.len() + 1;
+            let t2 = self.triangles.len();
+            let t3 = self.triangles.len() + 1;
             // add new triangles
-            triangles[t] = [p, b, c];
-            triangles.push([a, p, c]);
-            triangles.push([a, b, p]);
+            self.triangles[t] = [p, b, c];
+            self.triangles.push([a, p, c]);
+            self.triangles.push([a, b, p]);
             // add adjacency for new triangles
-            adjacencies[t] = [bc, t2*3+0, t3*3+0];
-            adjacencies.push([t1*3+1, ca, t3*3+1]);
-            adjacencies.push([t1*3+2, t2*3+2, ab]);
+            self.adjacencies[t] = [bc, t2*3+0, t3*3+0];
+            self.adjacencies.push([t1*3+1, ca, t3*3+1]);
+            self.adjacencies.push([t1*3+2, t2*3+2, ab]);
             // modify adjacency for adjacent triangles
-            if bc != BD { adjacencies[bc/3][bc%3] = t1*3+0; }
-            if ca != BD { adjacencies[ca/3][ca%3] = t2*3+1; }
-            if ab != BD { adjacencies[ab/3][ab%3] = t3*3+2; }
+            if bc != Self::BD { self.adjacencies[bc/3][bc%3] = t1*3+0; }
+            if ca != Self::BD { self.adjacencies[ca/3][ca%3] = t2*3+1; }
+            if ab != Self::BD { self.adjacencies[ab/3][ab%3] = t3*3+2; }
             // update grid
-            grid.insert(hash, t2);
-            grid.insert(hashes[a], t2);
-            // grid.insert(hashes[b], t1);
-            // grid.insert(hashes[c], t1);
+            self.grid.insert(hash, t2);
+            self.grid.insert(self.hashes[a], t2);
+            // self.grid.insert(self.hashes[b], t1);
+            // self.grid.insert(self.hashes[c], t1);
             (t1*3+0, t2*3+1, t3*3+2)
         };
 
@@ -218,50 +240,73 @@ pub fn delaunay(points: &[Point]) -> Vec<[usize; 3]> {
         let mut stack = vec![e1, e2, e3];
         while let Some(e) = stack.pop() {
             let (tri, side) = (e / 3, e % 3);
-            let e_ = adjacencies[tri][side];
-            if e_ == BD { continue; }
+            let e_ = self.adjacencies[tri][side];
+            if e_ == Self::BD { continue; }
             let (tri_, side_) = (e_ / 3, e_ % 3);
 
             // check if it is delaunay
-            let [a, b, c] = triangles[tri_];
-            if incirc(&points[a], &points[b], &points[c], point) != Ordering::Greater {
+            let [a, b, c] = self.triangles[tri_];
+            if incirc(&self.points[a], &self.points[b], &self.points[c], point) != Ordering::Greater {
                 continue;
             }
 
             // flip triangle
             
-            let a = triangles[tri][side];
-            let b = triangles[tri][(side + 1) % 3];
-            let c = triangles[tri_][side_];
-            let d = triangles[tri_][(side_ + 1) % 3];
+            let a = self.triangles[tri][side];
+            let b = self.triangles[tri][(side + 1) % 3];
+            let c = self.triangles[tri_][side_];
+            let d = self.triangles[tri_][(side_ + 1) % 3];
 
-            let da = adjacencies[tri][(side + 1) % 3];
-            let ab = adjacencies[tri][(side + 2) % 3];
-            let bc = adjacencies[tri_][(side_ + 1) % 3];
-            let cd = adjacencies[tri_][(side_ + 2) % 3];
+            let da = self.adjacencies[tri][(side + 1) % 3];
+            let ab = self.adjacencies[tri][(side + 2) % 3];
+            let bc = self.adjacencies[tri_][(side_ + 1) % 3];
+            let cd = self.adjacencies[tri_][(side_ + 2) % 3];
 
             // flip triangles
-            triangles[tri] = [a, b, c];
-            triangles[tri_] = [c, d, a];
-            adjacencies[tri] = [bc, tri_*3+1, ab];
-            adjacencies[tri_] = [da, tri*3+1, cd];
+            self.triangles[tri] = [a, b, c];
+            self.triangles[tri_] = [c, d, a];
+            self.adjacencies[tri] = [bc, tri_*3+1, ab];
+            self.adjacencies[tri_] = [da, tri*3+1, cd];
             // fix adjacencies
-            if ab != BD { adjacencies[ab/3][ab%3] = tri*3+2; }
-            if bc != BD { adjacencies[bc/3][bc%3] = tri*3+0; }
-            if cd != BD { adjacencies[cd/3][cd%3] = tri_*3+2; }
-            if da != BD { adjacencies[da/3][da%3] = tri_*3+0; }
+            if ab != Self::BD { self.adjacencies[ab/3][ab%3] = tri*3+2; }
+            if bc != Self::BD { self.adjacencies[bc/3][bc%3] = tri*3+0; }
+            if cd != Self::BD { self.adjacencies[cd/3][cd%3] = tri_*3+2; }
+            if da != Self::BD { self.adjacencies[da/3][da%3] = tri_*3+0; }
             // update grid
-            grid.insert(hashes[a], tri);
-            grid.insert(hashes[b], tri);
-            grid.insert(hashes[c], tri_);
-            grid.insert(hashes[d], tri_);
+            self.grid.insert(self.hashes[a], tri);
+            self.grid.insert(self.hashes[b], tri);
+            self.grid.insert(self.hashes[c], tri_);
+            self.grid.insert(self.hashes[d], tri_);
 
             // add flipped triangles to stack
             stack.push(tri*3+0);
             stack.push(tri_*3+2);
         }
     }
-    
-    // remove boundary
-    triangles.into_iter().filter(|&[a, b, c]| a < len && b < len && c < len).collect()
+
+    pub fn get_triangles(&self) -> Vec<[usize; 3]> {
+        // remove boundary
+        self.triangles.iter()
+            .cloned()
+            .filter(|&[a, b, c]| a >= 4 && b >= 4 && c >= 4)
+            .map(|[a, b, c]| [a - 4, b - 4, c - 4])
+            .collect()
+    }
+}
+
+pub fn delaunay(points: &[Point]) -> Vec<[usize; 3]> {
+    assert!(points.iter().all(|&(x, y)| x.is_finite() && y.is_finite()));
+    let bbox = {
+        let x_min = points.iter().map(|p| p.0).reduce(|a, b| a.min(b)).unwrap_or(0.0);
+        let y_min = points.iter().map(|p| p.1).reduce(|a, b| a.min(b)).unwrap_or(0.0);
+        let x_max = points.iter().map(|p| p.0).reduce(|a, b| a.max(b)).unwrap_or(0.0);
+        let y_max = points.iter().map(|p| p.1).reduce(|a, b| a.max(b)).unwrap_or(0.0);
+        let width = (x_max - x_min).max(y_max - y_min).max(MIN_WIDTH);
+        ((x_min, y_min), width)
+    };
+    let mut engine = Delaunay::new(bbox, points.len());
+    for point in points {
+        engine.add(point);
+    }
+    engine.get_triangles()
 }
